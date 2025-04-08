461c593477dbb53923b7c0b95956988e2f128f49
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-04-08 11:52:19 +0200

    quizzerrorrepare

--- src/Components/quizz.tsx
+++ src/Components/quizz.tsx
@@ -88,4 +88,31 @@ const data = [
   },
 ];
 
-export default data;
+const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [score, setScore] = useState(0);
+
+  const [showResult, setShowResult] = useState(false);
+
+  const handleAnswer = (selectedOption) => {
+    if (selectedOption === data[currentQuestion].answer) {
+      setScore(score + 1); // Augmente le score si la réponse est correcte
+    }
+
+    return (
+      <div className="quiz">
+        <div className="questionNumber">
+          Question : {currentQuestion + 1}
+          <b>/</b>
+          {data.length}
+        </div>
+        <div className="question">
+          question="{data[currentQuestion].question}" options="
+          {data[currentQuestion].options}" onanswer="{handleAnswer}"
+        </div>
+      </div>
+    );
+  };
+};
+export default App;
